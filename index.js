710f6e244a9a2c49f3e2bffdbbb7f9e584addec6
windam <windam@live>
2019-07-15 16:04:11 +0530

    setting the route for users, auth, profile and posts

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ connectDB();
 
 app.get("/", (req, res) => res.send("API Running"));
 
+// Define Routes
+app.use("/api/users", require("./route/api/users"));
+app.use("/api/auth", require("./route/api/auth"));
+app.use("/api/profile", require("./route/api/profile"));
+app.use("/api/posts", require("./route/api/posts"));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
