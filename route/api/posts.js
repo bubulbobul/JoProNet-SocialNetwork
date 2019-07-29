9a57c011bedef76518843071ca15c6752f5ca75a
windam <windam@live>
2019-07-29 20:04:19 +0530

    like a post

--- route/api/posts.js
+++ route/api/posts.js
@@ -100,10 +100,34 @@ router.delete("/:id", auth, async (req, res) => {
     res.json(post);
   } catch (err) {
     console.error(err.message);
-    console.error(err.message);
     if (err.kind === "ObjectId") {
       return res.status(404).json({ msg: "Post not found" });
     }
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route   PUT api/posts/like/:id
+// @desc    To like a post
+// @access  Private
+router.put("/like/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Check if the post has already been like by a same user
+    if (
+      post.likes.filter(like => like.user.toString() === req.user.id).length > 0
+    ) {
+      return res.status(400).json({ msg: "Post already liked" });
+    }
+
+    post.likes.unshift({ user: req.user.id });
+
+    await post.save();
+
+    res.json(post.likes);
+  } catch (error) {
+    console.error(err.message);
     res.status(500).send("Server Error");
   }
 });
