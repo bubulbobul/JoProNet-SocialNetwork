7c7cd9545b805492804055e5303635377609735e
windam <windam@live>
2019-07-29 19:40:24 +0530

    Create post scheman and add new post

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ app.use("/api/auth", require("./route/api/auth"));
 app.use("/api/profile", require("./route/api/profile"));
 app.use("/api/posts", require("./route/api/posts"));
 
+// npm run server
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
