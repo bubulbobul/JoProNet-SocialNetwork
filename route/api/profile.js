5c3e964b7379252c04f95de4740fc644c2bbbfee
windam <windam@live>
2019-07-29 15:11:54 +0530

    adding experience

--- route/api/profile.js
+++ route/api/profile.js
@@ -121,7 +121,7 @@ router.post(
   }
 );
 
-// @route   Get api/profile
+// @route   GET api/profile
 // @desc    To get all profiles
 // @access  Public
 router.get("/", async (req, res) => {
@@ -173,5 +173,66 @@ router.delete("/", auth, async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
+
+// @route   PUT api/profile/experience
+// @desc    Update profile by adding experience to a profile
+// @access  Private
+router.put(
+  "/experience",
+  [
+    auth,
+    [
+      check("title", "Title is required")
+        .not()
+        .isEmpty(),
+      check("company", "Company is required")
+        .not()
+        .isEmpty(),
+      check("from", "From date is required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    } = req.body;
+
+    const newExp = {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    };
+
+    try {
+      // Fisrt fetch the profile where we want toupdate
+      const profile = await Profile.findOne({ user: req.user.id });
+
+      profile.experience.unshift(newExp);
+
+      await profile.save();
+
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
