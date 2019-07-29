9843f899ee41753aab1ccc024f5f6445b75f11f4
windam <windam@live>
2019-07-29 20:21:28 +0530

    create a comment on a post

--- route/api/posts.js
+++ route/api/posts.js
@@ -162,4 +162,46 @@ router.put("/unlike/:id", auth, async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
+
+// @route   POST api/posts/comment/:id
+// @desc    Create a comment on a post
+// @access  Private
+router.post(
+  "/comment/:id",
+  [
+    auth,
+    check("text", "Text is required")
+      .not()
+      .isEmpty()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      // Get the user
+      const user = await User.findById(req.user.id).select("-password");
+      const post = await Post.findById(req.params.id);
+
+      const newComment = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+      };
+
+      post.comments.unshift(newComment);
+
+      await post.save();
+
+      res.json(post.comments);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
 module.exports = router;
