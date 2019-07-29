78ff740e9dc68d732067b1b4a0286f1ebd3f3a28
windam <windam@live>
2019-07-29 20:08:54 +0530

    unlike a post

--- route/api/posts.js
+++ route/api/posts.js
@@ -132,4 +132,34 @@ router.put("/like/:id", auth, async (req, res) => {
   }
 });
 
+// @route   PUT api/posts/unlike/:id
+// @desc    To unlike a post
+// @access  Private
+router.put("/unlike/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Check if the post has already been like
+    if (
+      post.likes.filter(like => like.user.toString() === req.user.id).length ===
+      0
+    ) {
+      return res.status(400).json({ msg: "Post has not yet been liked" });
+    }
+
+    // Get the remove index
+    const removeIndex = post.likes
+      .map(like => like.user.toString())
+      .indexOf(req.user.id);
+
+    post.likes.splice(removeIndex, 1);
+
+    await post.save();
+
+    res.json(post.likes);
+  } catch (error) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 module.exports = router;
