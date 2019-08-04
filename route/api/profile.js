47cb752bd4a7f1a1560f2c824e7852f977c8b903
windam <windam@live>
2019-08-04 10:07:10 +0530

    adding delete user post

--- route/api/profile.js
+++ route/api/profile.js
@@ -8,6 +8,7 @@ const { check, validationResult } = require("express-validator");
 
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
+const Post = require("../../models/Post");
 
 // @route   GET api/profile/me
 // @desc    To get profile of the current connected user
@@ -175,7 +176,9 @@ router.get("/user/:user_id", async (req, res) => {
 // @access  Private
 router.delete("/", auth, async (req, res) => {
   try {
-    // @Todo - remove users posts
+    // @Todo - remove user posts
+    // To remove posts
+    await Post.deleteMany({ user: req.user.id });
 
     // To remove a profile
     await Profile.findOneAndRemove({ user: req.user.id });
